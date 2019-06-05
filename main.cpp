424b9db6035e0af332a41421465760b5616729f3
Giovanni Martinelli <giofmartinelli@gmail.com>
2019-06-05 15:53:57 -0300

    Creating file complete

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
+
+void create_file()
+{
+    std::ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "Writing this to a file.\n";
+    myfile.close();
+}
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Creating File!" << std::endl;
+    create_file();
     return 0;
 }
